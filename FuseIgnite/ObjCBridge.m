025582ac79dab539829b531e49591cfbbc749031
Daniel Reilly <danielreilly0@gmail.com>
2015-10-05 19:50:09 +0100

    Adding bridging headers. I don't think I need both.

---
+++ FuseIgnite/ObjCBridge.m
@@ -0,0 +1,11 @@
+//
+//  ObjCBridge.m
+//  FuseIgnite
+//
+//  Created by Daniel Reilly on 05/10/2015.
+//  Copyright © 2015 Fuse Technology. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <Bolts/Bolts.h>
