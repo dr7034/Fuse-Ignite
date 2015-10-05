6ffb2def2a1fe0fa6dd974a892f13dc8f6e24ab8
Daniel Reilly <danielreilly0@gmail.com>
2015-10-05 19:49:10 +0100

    Adding register view controller.

---
+++ FuseIgnite/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  FuseIgnite
+//
+//  Created by Daniel Reilly on 05/10/2015.
+//  Copyright © 2015 Fuse Technology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    
+    
+}
