48e0ab524da4540df9c9ecd5b242085e64ccc549
Daniel Reilly <danielreilly0@gmail.com>
2015-12-07 18:59:05 +0000

    Adding reuse identifiers for all table views to stop warnings when building.

---
+++ FuseIgnite/NavigationMenuController.swift
@@ -0,0 +1,11 @@
+//
+//  NavigationMenuController.swift
+//  FuseIgnite
+//
+//  Created by Daniel Reilly on 12/11/2015.
+//  Copyright © 2015 Fuse Technology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
