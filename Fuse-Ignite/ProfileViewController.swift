daf3cb0265fdc029444b6c5f436e6416608d432b
Daniel Reilly <danielreilly0@gmail.com>
2016-04-11 22:12:52 +0100

    This is not the right way to go about this. It's awful. A stack or table view needs to be implemented for all of these sections such as in the event home

--- Fuse-Ignite/ProfileViewController.swift
+++ Fuse-Ignite/ProfileViewController.swift
@@ -19,6 +19,32 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var userBioTextView: UITextView!
     @IBOutlet weak var userNetworkingObjectivesTextView: UITextView!
     
+    //This is not the right way to go about this. It's awful. A stack or table view needs to be implemented for all of these sections such as in the event home
+    
+    //Talk to me about
+    @IBOutlet weak var userSubject0: UILabel!
+    @IBOutlet weak var userSubject1: UILabel!
+    @IBOutlet weak var userSubject2: UILabel!
+    @IBOutlet weak var userSubject3: UILabel!
+    @IBOutlet weak var userSubject4: UILabel!
+    @IBOutlet weak var userSubject5: UILabel!
+    @IBOutlet weak var userSubject6: UILabel!
+    @IBOutlet weak var userSubject7: UILabel!
+    
+    
+    //Phrases Around Me
+    @IBOutlet weak var userPhraseAround0: UILabel!
+    @IBOutlet weak var userPhraseAround1: UILabel!
+    @IBOutlet weak var userPhraseAround2: UILabel!
+    @IBOutlet weak var userPhraseAround3: UILabel!
+    
+    //What Motivates Me
+    @IBOutlet weak var userMotivateMe0: UILabel!
+    @IBOutlet weak var userMotivateMe1: UILabel!
+    @IBOutlet weak var userMotivateMe2: UILabel!
+    @IBOutlet weak var userMotivateMe3: UILabel!
+    
+    
     // Container to store the view table selected object
     var currentObject : PFObject?
     
